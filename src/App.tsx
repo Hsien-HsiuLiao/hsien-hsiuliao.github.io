1cd21921c4426df0450246b74d743d2039fe9cb5
hsien <hsien_hsiu.liao@yahoo.com>
2025-01-31 11:52:22 -0800

    add Globalstyle and Main components

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,26 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
 import { Header } from './components/Header/Header.tsx'
+import { Main } from './components/Main/Main.tsx'
+import { GlobalStyle } from './styles/global.ts'
 
 
 function App() {
   return (
-    <div className="App">
+    /*  <div className="App"> */
+    <>
+      <GlobalStyle></GlobalStyle>
       <Header></Header>
-      <header className="App-header">
+      <Main></Main>
+      {/*   <header className="App-header">
         
         <p>
           Hsien-Hsiu's portfolio
         </p>
         
-      </header>
-    </div>
+      </header> */}
+      {/*    </div>  */}
+    </>
   );
 }
 
