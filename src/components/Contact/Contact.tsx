c2cceb062541733e77aa76a934a4254e69d2e6d6
hsien <hsien_hsiu.liao@yahoo.com>
2025-02-10 17:28:18 -0800

    add LinkedIn to Contact

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -11,17 +11,18 @@ export function Contact(){
     <Container id="contato">
       <header>
         <h2>Contact</h2>
-        <p>Feel free to send me a message.</p>
+        {/* <p>Feel free to send me a message.</p> */}
       </header>
       <div className="contacts">
         <div>
          {/*  <img src={emailIcon} alt="Email" />
           <a href="mailto:joaotuliosoares@hotmail.com">joaotuliosoares@hotmail.com</a> */}
+          LinkedIn
         </div>
-        <div>
-        {/*   <img src={phoneIcon} alt="Email" />
-          <a href="tel:+5531981213843">(31) 98121-3843</a> */}
-        </div>  
+       {/*  <div>
+          <img src={phoneIcon} alt="Email" />
+          <a href="tel:+5531981213843">(31) 98121-3843</a>
+        </div>   */}
       </div>
    {/*    <Form></Form> */}
     </Container>
